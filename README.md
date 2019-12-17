79ecfb71c59baf5dbb0ffdfab9aa421783c8d7cc
svanderwal2 <56695978+svanderwal2@users.noreply.github.com>
2019-12-17 05:54:07 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+## Landing Page - Dynamically Built Nav
+
+This project is a multi-section landing page with a dynamically updating navigational menu written in javasrcipt. The content of the nav menu will automatically update based on new sections added to the page.
+
+##### Authors: 
+Sean van der Wal with Udacity template code for HTML and CSS. All Javasrcipt code written by Sean van der Wal. 
--- js/app.js
+++ js/app.js
@@ -1,15 +1,8 @@
-/**
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
-*/
-
 // Global Variables 
 
 const pageSections = document.querySelectorAll("section");
 const navbar = document.getElementById("navbar__list");
-const navbarLi = document.querySelectorAll("li");
+const navLi = document.getElementsByTagName("li");
 
 // Build Nav
 
@@ -22,35 +15,22 @@ for (let i = 0; i < pageSections.length; i++) {
 
 // Add class 'active' to section when near top of viewport
 
-for (let i = 0; i < pageSections.length; i++) {
-    function inViewport(pageSections) {
-        let bounding = pageSections.getBoundingClientRect();
-        return (
-            bounding.top >= 0 &&
-            bounding.left >= 0 &&
-            bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
-    };
-
-    window.addEventListener("scroll", function(event) {
-        if (inViewport(pageSections)) {
-            pageSections.classList.add("active");
-        } else {
-            pageSections.classList.remove("active");
-        }
-    });
+function inViewport(element) {
+    let bounding = element.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
 };
 
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
+window.addEventListener("scroll", function() {
+    for (const section of pageSections) {
+        if (inViewport(section)) {
+            section.classList.add("active");
+        } else {
+            section.classList.remove("active");
+        }
+    }
+});
